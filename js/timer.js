03e295f23ed95d7ee117e335e0935aaadb26ccf2
366M <106433084+366M@users.noreply.github.com>
2023-01-26 22:13:19 +0100

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -16,7 +16,7 @@ function odliczanie()
 		if (sekunda<10) sekunda = "0"+sekunda;
 		
 		document.getElementById("zegar").innerHTML = 
-		 dzien+"."+miesiac+"."+rok+" |=+-XYZ-+=| "+godzina+":"+minuta+":"+sekunda;
+		+godzina+":"+minuta+":"+sekunda"  "dzien+"."+miesiac+"."+rok+;
 		 
 		 setTimeout("odliczanie()",1000);
 	}
